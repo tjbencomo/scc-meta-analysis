63f80317f43d87fe069e75abc710e54057a27c6f
Tomas Bencomo <tjbencomo@gmail.com>
2024-03-06 12:02:48 -0700

    Add FASTQ downloader files

---
+++ src/download_sra.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=download_sra
+#SBATCH --output=
+#SBATCH --nodes=1
+#SBATCH --time=02-00:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=14000
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=
+
+set -euo pipefail
+
+ml system parallel
+fqdir=$SCRATCH/nmsc-data
+mkdir -p $fqdir
+cd $fqdir
+# srr_file=$SCRATCH/nmsc-data/SRA_Remaining_Samples.txt
+srr_file=$HOME/nmsc-rna-seq/data/srr_ids.txt
+cat $srr_file | parallel -j 12 'fasterq-dump {}'
